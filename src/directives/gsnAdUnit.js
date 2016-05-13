98f3a16e9f842ea828498ded6c28351eb48edbc6
noogen <tom@noogen.net>
2016-05-13 09:15:09 -0500

    remove load ads dependencies

--- src/directives/gsnAdUnit.js
+++ src/directives/gsnAdUnit.js
@@ -1,8 +1,8 @@
-﻿(function (angular, undefined) {
+﻿(function(angular, undefined) {
   'use strict';
   var myModule = angular.module('gsn.core');
 
-  myModule.directive('gsnAdUnit', ['gsnStore', '$timeout', 'gsnApi', '$rootScope', '$http', '$templateCache', '$interpolate', function (gsnStore, $timeout, gsnApi, $rootScope, $http, $templateCache, $interpolate) {
+  myModule.directive('gsnAdUnit', ['gsnStore', '$timeout', 'gsnApi', '$rootScope', '$http', '$templateCache', '$interpolate', function(gsnStore, $timeout, gsnApi, $rootScope, $http, $templateCache, $interpolate) {
     // Usage: create an adunit and trigger ad refresh
     // 
     // Creates: 2014-04-05 TomN
@@ -16,9 +16,12 @@
     function link(scope, elm, attrs) {
       scope.templateHtml = null;
       var tileId = gsnApi.isNull(attrs.gsnAdUnit, '');
+
       if (tileId.length > 0) {
         var templateUrl = gsnApi.getThemeUrl('/../common/views/tile' + tileId + '.html');
-        var templateLoader = $http.get(templateUrl, { cache: $templateCache });
+        var templateLoader = $http.get(templateUrl, {
+          cache: $templateCache
+        });
         var hasTile = false;
 
         templateLoader.success(function(html) {
@@ -27,8 +30,13 @@
       }
 
       function linkTile() {
+        // ignore duplicate tile
+        if (tileId == 5) {
+          return;
+        }
+
         if (tileId.length > 0) {
-          if (hasTile && scope.templateHtml) {
+          if (scope.templateHtml) {
             elm.html(scope.templateHtml);
             var html = $interpolate(scope.templateHtml)(scope);
             elm.html(html);
@@ -37,35 +45,13 @@
             $rootScope.$broadcast('gsnevent:loadads');
           }
         } else {
-          if (hasTile) {
-            // find adunit
-            elm.find('.gsnadunit').addClass('brickunit');
-            
-            // broadcast message
-            $rootScope.$broadcast('gsnevent:loadads');
-          }
-        }
-      }      
+          // find adunit
+          elm.find('.gsnadunit').addClass('brickunit');
 
-      gsnStore.getAdPods().then(function(rsp) {
-        if (rsp.success) {
-          // check if tile is in response
-          // rsp.response;
-          if (attrs.gsnAdUnit) {
-            for (var i = 0; i < rsp.response.length; i++) {
-              var tile = rsp.response[i];
-              if (tile.Code == attrs.gsnAdUnit) {
-                hasTile = true;
-                break;
-              }
-            }
-          } else {
-            hasTile = true;
-          }
-
-          linkTile();
+          // broadcast message
+          $rootScope.$broadcast('gsnevent:loadads');
         }
-      });
+      }
     }
   }]);
 })(angular);
