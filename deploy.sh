825c6de33cd03647fbf5cacde77552c626bfdfcc
noogen <tom@noogen.net>
2016-06-11 18:29:01 -0500

    move everything

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-cp gsncore-basic.js ./src
-cp gsncore-basic.min.js ./src
-cp gsncore.js ./src
-cp gsncore.min.js ./src
-aws s3 sync "./src" "s3://brick-web/ds/$1/script/gsncore/latest"
+aws s3 sync "./" "s3://brick-web/ds/$1/script/gsncore/latest"
