350ea395e04b2a90f5531e91d9e2152afdfa1171
Tom Noogen <tom@Toms-iMac.local>
2016-08-03 09:30:02 -0500

    remove bad directive

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.8.35
+ * version 1.8.36
  * gsncore repository
- * Build date: Wed Aug 03 2016 08:51:00 GMT-0500 (CDT)
+ * Build date: Wed Aug 03 2016 09:29:48 GMT-0500 (CDT)
  */
 ;(function() {
   'use strict';
@@ -9312,44 +9312,6 @@
       return e.attr('content', v);
     }
   });
-})(angular);
-
-(function(angular, undefined) {
-  'use strict';
-  var myModule = angular.module('gsn.core');
-
-  myModule.directive('ngGiveHead', [function() {
-    // Usage: ability to add to head element.  Becareful, since only one element is valid, this should only be use in layout html.
-    //
-    // Creates: 2013-12-12 TomN
-    //
-    var directive = {
-      restrict: 'EA',
-      link: link
-    };
-    return directive;
-
-    function link(scope, element, attrs) {
-      // attempt to add element to head
-      var el = angular.element('<' + attrs.ngGiveHead + '>');
-      if (attrs.attributes) {
-        var myAttrs = scope.$eval(attrs.attributes);
-        angular.forEach(myAttrs, function(v, k) {
-          el.attr(k, v);
-        });
-      }
-
-      var pNode = angular.element('head')[0];
-      pNode.insertBefore(el[0], angular.element('title')[0]);
-
-      // When we go out of scope restore the original value.
-      scope.$on('$destroy', function() {
-        if (attrs.remove) {
-          pnode.removeChild(el[0]);
-        }
-      });
-    }
-  }]);
 })(angular);
 
 (function (angular, undefined) {
