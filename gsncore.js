488fcc5fa7d5a333cb86464d4adf4d0ca822639e
Babumohankumar <bmohankumar@trybrick.com>
2016-09-27 20:14:58 +0530

    Address split and Popup for Product detail on click

--- gsncore.js
+++ gsncore.js
@@ -9983,16 +9983,22 @@ var mod;mod=angular.module("infinite-scroll",[]),mod.directive("infiniteScroll",
     function getPrimaryAddress(householdField) {
       if ((gsnApi.isNull(householdField, null) !== null) && (gsnApi.isNull(householdField.Addresses, null) !== null)) {
         // Assign the primary address
-		if(householdField.Addresses.Address.length == undefined){
-			$scope.primaryLoyaltyAddress=householdField.Addresses.Address;	
-		}
-		else{
-			$scope.primaryLoyaltyAddress=householdField.Addresses.Address[0];	
-		}
-		
+        var _address=null;
+    		if(householdField.Addresses.Address.length == undefined){
+          _address=householdField.Addresses.Address;
+        }
+        else{
+          _address=householdField.Addresses.Address[0]; 
+        }
+        if(_address!=null){
+    			$scope.primaryLoyaltyAddress=_address;
+          if(_address.StreetAddress.includes('\n')){
+            $scope.primaryLoyaltyAddress.StreetAddress=_address.StreetAddress.split('\n')[0].trim();
+            $scope.primaryLoyaltyAddress.StreetAddress2=_address.StreetAddress.split('\n')[1].trim();
+          }
+        }
       }
     }
-
     ////
     // Get Promotion Value
     ////
@@ -12981,7 +12987,7 @@ var mod;mod=angular.module("infinite-scroll",[]),mod.directive("infiniteScroll",
 
     function link(scope, element, attrs) {
       var text = '',
-          title = attrs.title || '';
+          title = attrs.popovertitle || '';
 
       // wait until finish interpolation
       $timeout(function () {
@@ -13040,6 +13046,22 @@ var mod;mod=angular.module("infinite-scroll",[]),mod.directive("infiniteScroll",
       });
     }
   }]);
+})(angular);
+
+(function (angular, undefined) {
+  'use strict';
+  var myModule = angular.module('gsn.core');
+
+ myModule.directive('gsnPopoverDialog', ['gsnApi', '$localStorage', '$compile', function(gsnApi, $localStorage, $compile) {
+  return  {
+    restrict : 'EA',
+    template:'<div id="myProductDetailsModal" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">{{productData.Description}}</h4> </div><div class="modal-body"><div class="row" style="border-top:0;" ><div class="col-md-6"><img ng-show="productData.ImageUrl" data-ng-src="{{productData.ImageUrl}}" alt="[Product Image]" style="max-width: 200px; max-height: 200px;min-width: 200px; min-height: 200px"/></div><div class="col-md-6"><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>UPC:</label></div><div class="col-md-6 align-left"><label>{{productData.UPC11}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>Size:</label></div><div class="col-md-6 align-left"><label>{{productData.ItemExtendedSize}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>SalePrice:</label></div><div class="col-md-6 align-left"><label ng-show="productData.SalePrice">${{productData.SalePrice.toFixed(2)}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>Brand:</label></div><div class="col-md-6 align-left"><label>{{productData.BrandName}}</label></div></div></div></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>',
+             
+    link:function(scope,element,attrs) {
+    }
+  }
+  }]);
+
 })(angular);
 
 (function (angular, undefined) {
