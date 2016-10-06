697855c8e58fa9b4b385f3f58e13907dca4badec
Tom Noogen <tom@tnguyen-imac.local>
2016-10-06 15:26:50 -0500

    update build

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -2,7 +2,7 @@
  * gsncore
  * version 1.8.41
  * gsncore repository
- * Build date: Fri Sep 23 2016 13:41:44 GMT-0500 (CDT)
+ * Build date: Thu Oct 06 2016 15:25:30 GMT-0500 (CDT)
  */
 ;(function() {
   'use strict';
@@ -7565,9 +7565,11 @@
         }
       }
 
-      scope.closeModal = function() {
+      scope.closeModal = function(shouldReload) {
 	    if(timeoutOfOpen != null)
-		  $timeout.cancel(timeoutOfOpen);
+		    $timeout.cancel(timeoutOfOpen);
+		if(shouldReload!=undefined && shouldReload)
+	       window.top.location.reload();
         return gmodal.hide();
       };
 
@@ -8004,6 +8006,21 @@
   }]);
 })(angular);
 
+(function (angular, undefined) {
+  'use strict';
+  var myModule = angular.module('gsn.core');
+
+ myModule.directive('gsnPopoverDialog', ['gsnApi', '$localStorage', '$compile', function(gsnApi, $localStorage, $compile) {
+  return  {
+    restrict : 'EA',
+    template:'<div id="myProductDetailsModal" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">{{productData.Description}}</h4> </div><div class="modal-body"><div class="row" style="border-top:0;" ><div class="col-md-6"><img ng-show="productData.ImageUrl" data-ng-src="{{productData.ImageUrl}}" alt="[Product Image]" style="max-width: 200px; max-height: 200px;min-width: 200px; min-height: 200px"/></div><div class="col-md-6"><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>UPC:</label></div><div class="col-md-6 align-left"><label>{{productData.UPC11}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>Size:</label></div><div class="col-md-6 align-left"><label>{{productData.ItemExtendedSize}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>SalePrice:</label></div><div class="col-md-6 align-left"><label ng-show="productData.SalePrice">${{productData.SalePrice.toFixed(2)}}</label></div></div><div class="row" style="border-top:0;"><div class="col-md-6 align-left"><label>Brand:</label></div><div class="col-md-6 align-left"><label>{{productData.BrandName}}</label></div></div></div></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>',
+             
+    link:function(scope,element,attrs) {
+    }
+  }
+  }]);
+
+})(angular);
 (function (angular, undefined) {
   'use strict';
   var myModule = angular.module('gsn.core');
