ebe221de12fbe952d47775c82f01d586595f1f37
Tom Noogen <tom@noogen.net>
2017-06-29 19:40:12 -0500

    put back advertising

--- gsncore.js
+++ gsncore.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.10.47
+ * version 1.10.48
  * gsncore repository
- * Build date: Thu Jun 29 2017 14:21:49 GMT-0500 (CDT)
+ * Build date: Thu Jun 29 2017 19:39:39 GMT-0500 (CDT)
  */
 ( function () {
   'use strict';
@@ -3950,14 +3950,18 @@ var mod;mod=angular.module("infinite-scroll",[]),mod.directive("infiniteScroll",
 ( function ( angular, undefined ) {
   'use strict';
   var serviceId = 'gsnAdvertising';
-  angular.module( 'gsn.core' ).service( serviceId, [ '$timeout', '$location', 'gsnProfile', 'gsnApi', '$window', gsnAdvertising ] );
+  angular.module( 'gsn.core' ).service( serviceId, [ '$timeout', '$location', 'gsnProfile', 'gsnApi', '$window', 'debounce', '$rootScope', 'gsnStore', gsnAdvertising ] );
 
-  function gsnAdvertising( $timeout, $location, gsnProfile, gsnApi, $window ) {
-    var returnObj = {};
+  function gsnAdvertising( $timeout, $location, gsnProfile, gsnApi, $window, debounce, $rootScope, gsnStore ) {
+    var service = {
+      forceRefresh: true,
+      actionParam: null,
+      doRefresh: debounce( doRefresh, 500 )
+    };
     var bricktag = $window.bricktag;
 
     if ( !bricktag )
-      return returnObj;
+      return service;
 
     bricktag.on( 'clickRecipe', function ( data ) {
       $timeout( function () {
@@ -4006,7 +4010,84 @@ var mod;mod=angular.module("infinite-scroll",[]),mod.directive("infiniteScroll",
       } );
     } );
 
-    return returnObj;
+    function shoppingListItemChange( event, shoppingList, item ) {
+      var currentListId = gsnApi.getShoppingListId();
+      if ( shoppingList.ShoppingListId === currentListId ) {
+        var cat = gsnStore.getCategories()[ item.CategoryId ];
+        bricktag.addDept( cat.CategoryName );
+        // service.actionParam = {evtname: event.name, dept: cat.CategoryName, pdesc: item.Description, pcode: item.Id, brand: item.BrandName};
+        service.doRefresh();
+      }
+    }
+
+    $rootScope.$on( 'gsnevent:shoppinglistitem-updating', shoppingListItemChange );
+    $rootScope.$on( 'gsnevent:shoppinglistitem-removing', shoppingListItemChange );
+    $rootScope.$on( 'gsnevent:shoppinglist-loaded', function ( event, shoppingList, item ) {
+      var list = gsnProfile.getShoppingList();
+      if ( list ) {
+        // load all the ad depts
+        var items = gsnProfile.getShoppingList().allItems();
+        var categories = gsnStore.getCategories();
+
+        angular.forEach( items, function ( item, idx ) {
+          if ( gsnApi.isNull( item.CategoryId, null ) === null ) return;
+
+          if ( categories[ item.CategoryId ] ) {
+            var newKw = categories[ item.CategoryId ].CategoryName;
+            bricktag.addDept( newKw );
+          }
+        } );
+
+        // service.actionParam = {evtname: event.name, evtcategory: gsnProfile.getShoppingListId() };
+      }
+    } );
+
+    $rootScope.$on( '$locationChangeSuccess', function ( event, next ) {
+      var currentPath = angular.lowercase( gsnApi.isNull( $location.path(), '' ) );
+      gsnProfile.getProfile().then( function ( p ) {
+        var isLoggedIn = gsnApi.isLoggedIn();
+
+        bricktag.setDefault( {
+          page: currentPath,
+          storeid: gsnApi.getSelectedStoreId(),
+          consumerid: gsnProfile.getProfileId(),
+          isanon: !isLoggedIn,
+          loyaltyid: p.response.ExternalId
+        } );
+      } );
+      service.forceRefresh = true;
+      service.doRefresh();
+    } );
+
+    $rootScope.$on( 'gsnevent:loadads', function ( event, next ) {
+      service.actionParam = {
+        evtname: event.name
+      };
+      service.doRefresh();
+    } );
+
+    $rootScope.$on( 'gsnevent:digitalcircular-pagechanging', function ( event, data ) {
+      // service.actionParam = {evtname: event.name, evtcategory: data.circularIndex, pdesc: data.pageIndex};
+      service.doRefresh();
+    } );
+
+    init();
+
+    // initialization
+    function init() {
+      if ( service.isIE ) {
+        bricktag.minSecondBetweenRefresh = 15;
+      }
+    }
+
+    // refresh method
+    function doRefresh() {
+      ( $rootScope.gvm || {} ).adsCollapsed = false;
+      bricktag.refresh( service.actionParam, service.forceRefresh );
+      service.forceRefresh = false;
+    }
+
+    return service;
   }
 } )( angular );
 
