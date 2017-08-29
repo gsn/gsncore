b7d3cee82ffe4dc74a3398f1d843cdc3177e9565
Tom Noogen <tom@noogen.net>
2017-08-29 12:02:21 -0500

    add registration

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.11.16
+ * version 1.11.17
  * gsncore repository
- * Build date: Tue Aug 29 2017 10:11:57 GMT-0500 (CDT)
+ * Build date: Tue Aug 29 2017 12:02:09 GMT-0500 (CDT)
  */
 (function() {
   'use strict';
@@ -6695,6 +6695,8 @@
         currentPath = '/article';
       } else if (currentPath.indexOf('/recipevideo/') > -1) {
         currentPath = '/recipevideo';
+      } else if (currentPath.indexOf('/registration/') > -1) {
+        currentPath = '/registration';
       } else if (currentPath.indexOf('/store/') > -1) {
         currentPath = '/store';
       }
