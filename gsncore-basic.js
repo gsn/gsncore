da478021f591af0280fa5cb4db06164c10d8b7c9
noogen <tom@noogen.net>
2019-08-13 16:12:43 -0500

    fix store locations

--- gsncore-basic.js
+++ gsncore-basic.js
@@ -1,8 +1,8 @@
 /*!
  * gsncore
- * version 1.12.57
+ * version 1.12.58
  * gsncore repository
- * Build date: Tue Aug 13 2019 15:55:04 GMT-0500 (Central Daylight Time)
+ * Build date: Tue Aug 13 2019 16:12:34 GMT-0500 (Central Daylight Time)
  */
 (function() {
   'use strict';
